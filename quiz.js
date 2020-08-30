ce1f36b68eed83660278c6c7da060a23b095c506
Devan Wong <devan.wong@yahoo.com>
2020-08-30 09:45:14 -0700

    devan css styles

---
+++ quiz.js
@@ -0,0 +1,67 @@
+// const quizContainer = document.getElementById('quiz');
+// const resultsContainer = document.getElementById('results');
+// const submitButton = document.getElementById('submit');
+
+
+// function buildQuiz(){
+
+// }
+
+
+// function showResults(){
+
+// }
+
+// buildQuiz();
+
+// submitButton.addEventListener('click', showResults);
+
+
+// const myQuestions = [
+//     {
+//         question: "What is Javascript?",
+//         answers : {
+//             a: "",
+//             b: "",
+//             c: ""
+//     },
+//         correctAnswer: "c"
+//     },
+//     {
+//         question: "What is Javascript?",
+//         answers : {
+//             a: "",
+//             b: "",
+//             c: ""
+//     },
+//         correctAnswer: "c"
+//     },
+//     {
+//         question: "What is Javascript?",
+//         answers : {
+//             a: "",
+//             b: "",
+//             c: ""
+//     },
+//         correctAnswer: "c"
+//     },
+//     {
+//         question: "What is Javascript?",
+//         answers : {
+//             a: "",
+//             b: "",
+//             c: ""
+//     },
+//         correctAnswer: "c"
+//     },
+//     {
+//         question: "What is Javascript?",
+//         answers : {
+//             a: "",
+//             b: "",
+//             c: ""
+//     },
+//         correctAnswer: "c"
+//     }
+    
+// ]
