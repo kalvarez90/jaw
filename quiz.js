988ffc139d60992937d54b31e6bc3f8ba4af891b
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-09-04 21:03:00 -0400

    updated

--- quiz.js
+++ quiz.js
@@ -3,65 +3,109 @@
 // const submitButton = document.getElementById('submit');
 
 
-// function buildQuiz(){
+function buildQuiz(){
 
-// }
-
-
-// function showResults(){
-
-// }
-
-// buildQuiz();
-
-// submitButton.addEventListener('click', showResults);
+}
 
 
-// const myQuestions = [
-//     {
-//         question: "What is Javascript?",
-//         answers : {
-//             a: "",
-//             b: "",
-//             c: ""
-//     },
-//         correctAnswer: "c"
-//     },
-//     {
-//         question: "What is Javascript?",
-//         answers : {
-//             a: "",
-//             b: "",
-//             c: ""
-//     },
-//         correctAnswer: "c"
-//     },
-//     {
-//         question: "What is Javascript?",
-//         answers : {
-//             a: "",
-//             b: "",
-//             c: ""
-//     },
-//         correctAnswer: "c"
-//     },
-//     {
-//         question: "What is Javascript?",
-//         answers : {
-//             a: "",
-//             b: "",
-//             c: ""
-//     },
-//         correctAnswer: "c"
-//     },
-//     {
-//         question: "What is Javascript?",
-//         answers : {
-//             a: "",
-//             b: "",
-//             c: ""
-//     },
-//         correctAnswer: "c"
-//     }
-    
-// ]
+function showResults(){
+
+}
+
+ buildQuiz();
+
+submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+  {
+    question: "What is Javascript?",
+    answers : {
+       a: "",
+       b: "",
+       c: ""
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      answers : {
+         a: "<javascript>",
+         b: "<js>",
+         c: "<script>",
+         d: "<Jscript>"
+        },
+        correctAnswer: "c"
+      },
+        {
+          question: "How do you call a function named 'fullName'?",
+          answers : {
+            a: "call fullName",
+            b: "fullName()",
+            c: "call function fullName"
+           },
+           correctAnswer: "b"
+        },
+        {
+          question: "How does a for loop start?",
+          answers : {
+             a: "for(x = 0; x < 5; x++)",
+             b: "for(x <= 5; x++)",
+             c: "for(x = 0; x <= 5)"
+            },
+            correctAnswer: "a"
+          },
+          {
+            question: "How do you add a comment in JavaScript?",
+            answers : {
+               a: "<!--This is a comment -->",
+               b: "'This is a comment'",
+               c: "//This is a comment"
+              },
+              correctAnswer: "c"
+            },
+            {
+              question: "How does a for loop start?",
+              answers : {
+                 a: "for(x = 0; x < 5; x++)",
+                 b: "",
+                 c: ""
+                },
+                correctAnswer: "c"
+              },
+              {
+                question: "How does a for loop start?",
+                answers : {
+                   a: "for(x = 0; x < 5; x++)",
+                   b: "",
+                   c: ""
+                  },
+                  correctAnswer: "c"
+                },
+                {
+                  question: "How does a for loop start?",
+                  answers : {
+                     a: "for(x = 0; x < 5; x++)",
+                     b: "",
+                     c: ""
+                    },
+                    correctAnswer: "c"
+                  },
+                  {
+                    question: "How does a for loop start?",
+                    answers : {
+                       a: "for(x = 0; x < 5; x++)",
+                       b: "",
+                       c: ""
+                      },
+                      correctAnswer: "c"
+                    },
+                    {
+                      question: "How does a for loop start?",
+                      answers : {
+                         a: "for(x = 0; x < 5; x++)",
+                         b: "",
+                         c: ""
+                        },
+                        correctAnswer: "c"
+                      },
+        ]
